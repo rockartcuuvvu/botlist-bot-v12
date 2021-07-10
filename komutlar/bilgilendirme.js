6ff2f3913faeb6cb0b083df0b6f4aad169ad46e4
rockartcuuvvu <77324273+rockartcuuvvu@users.noreply.github.com>
2021-07-10 20:12:52 +0200

    English 6

--- komutlar/bilgilendirme.js
+++ komutlar/bilgilendirme.js
@@ -9,41 +9,41 @@ const motion42 = new Discord.MessageEmbed()
   .setColor('#f698d4')
   .setThumbnail("https://cdn.discordapp.com/attachments/815177653162082337/815581805888798741/bc05d5d7859ba639aee30d2e38455739.png")
   .setTimestamp()
-  .setFooter(`MoTion42 List`,client.user.avatarURL())  
+  .setFooter(`MoTion42 List`,client.user.avatarURL())
   .setAuthor(client.user.username,"https://cdn.discordapp.com/attachments/815177653162082337/815581805888798741/bc05d5d7859ba639aee30d2e38455739.png")
   .setDescription(`
-:pushpin:                                            **__Bot List Kurallar__**                                            :pushpin:
+:pushpin: **__Bot List Rules__** :pushpin:
 
-\`1)\` Botunuzda En Az 25 Komut Olmalıdır.
+\`1)\` Your bot must have at least 25 commands.
 
-\`2)\` Botunuzun En Az 25 Gün Önce Açılması Gerek 
+\`2)\` Your bot must be opened at least 25 days ago
 
-\`3)\` Dm-Duyuru Komutu Olmamalıdır.
+\`3)\` Should Not Be a Dm-Announcement Command.
 
-\`4)\` Botunuz Ban Botu Olmamalıdır.
+\`4)\` Your bot should not be a Ban bot.
 
-\`5)\` Ailemiz Top10 Tarzı Komutlar Olmamalıdır.
+\`5)\` Our Family Shouldn't Be Top10 Style Commands.
 
-\`6)\` Botunuz Saldırı Botu Olmamalıdır.
+\`6)\` Your bot must not be an Attack Bot.
 
-\`7)\` AntiSpam ve Dm Hg BB komutları ayarlamalı olmalıdır.
+\`7)\` AntiSpam and Dm Hg BB commands should be set.
 
-\`8)\` \`90\` Saatten Fazla Çevrimdışı Olmamalıdır
+\`8)\` Must Not Be Offline For More Than 90\` Hours
 
-\`9)\` Botunuz en az \`40.000\` kullanıcı ve \`40\` sunucu olmalıdır.
+\`9)\` Your bot must have at least \`40.000\` users and \`40\` servers.
 
-\`10)\` Botunuzu Ekledikten Sonra Yetkililere Ne Zaman Kontrol Edeceksiniz , Şu Kadar Zaman Oldu Gibi Yazmak Ban Sebebidir.
+\`10)\` When You Will Check The Authorities After Adding Your Bot, Writing It Like It Has Been Banned.
 
-\`11)\` \`b!bot-ekle\` komutunu spamlayak yasak. \`(Karaliste alınırsınız)\`
+\`11)\` Spamming the command \`b!bot-add\` is prohibited. \`(You will be blacklisted)\`
 
-\`12)\` Botunuzda everyone - here ve ya her hangi bi rolü herkese açık şekilde etiketlemek varsa açık şekilde botunuz onaylanmaz.
+\`12)\` If your bot has to publicly tag everyone - here or any role, your bot will not be approved explicitly.
 
-:gear: **__Bot Nasıl Eklenir?__** :gear:
+:gear: **__How to Add Bot?__** :gear:
 
-<#815285905061052426> Kanalına gelib \`b!bot-ekle\` \`bot-id\` \`prefix\` \`dbl-onay-durumu\` şeklinde başvuru yolluyorsunuz.
+You come to <#815285905061052426> and submit an application as \`b!bot-add\` \`bot-id\` \`prefix\` \`dbl-approval-state\`
 
-:dizzy: \`Not:\` Botunuz Onaylı İse Direk Eklenir.
-:dizzy: \`Not2:\` Botunuz **DBL** onaylıysa direk onaylanır. 
+:dizzy: \`Note:\` If your bot is approved, it will be added directly.
+:dizzy: \`Note2:\` If your bot is **DBL** approved, it will be approved directly.
 
 `)
  
@@ -58,7 +58,7 @@ exports.conf = {
 }
 
 exports.help = {
-    name: 'bilgilendirme',
-    description: 'bilgilendirme',
-    usage: 'bilgilendirme'
+    name: 'notify',
+    description: 'information',
+    usage: 'notify'
 }
